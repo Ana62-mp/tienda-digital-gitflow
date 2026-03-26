1ad0af04ebafb4b42d43ba2ed6144c7b282a9a87
joshTer-c2007 <jhosep.07txx@gmail.com>
2026-03-25 20:55:51 -0500

    crear atributos .java

--- Producto.java
+++ Producto.java
@@ -1,5 +1,16 @@
-package tienda-digital-gitflow;
-
 public class Producto {
-    
+    // Atributos privados según el estándar POO [cite: 87, 93]
+    private String nombre;
+    private double precio;
+    private int stock;
+    private String categoria;
+
+    // Constructor que asigna los parámetros con 'this' [cite: 33, 94]
+    public Producto(String nombre, double precio, int stock, String categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.categoria = categoria;
+    }
+
 }
