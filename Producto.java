c3fcbe93c32ebd4bab4e59f454a35cfbdf9dcdf1
joshTer-c2007 <jhosep.07txx@gmail.com>
2026-03-25 20:57:08 -0500

    añadir implementos .java

--- Producto.java
+++ Producto.java
@@ -12,5 +12,29 @@ public class Producto {
         this.stock = stock;
         this.categoria = categoria;
     }
+    public void mostrarInfo() {
+        System.out.println("Producto: " + nombre);
+        System.out.println("Categoria: " + categoria);
+        System.out.println("Precio: $" + precio);
+        System.out.println("Stock: " + stock + " unidades");
+    }
 
+    // Calcula descuento sin modificar el atributo precio [cite: 36]
+    public void aplicarDescuento(double porcentaje) {
+        double montoDescontado = precio * (porcentaje / 100);
+        double precioFinal = precio - montoDescontado;
+        
+        System.out.println("Descuento del " + porcentaje + "%: -$" + montoDescontado);
+        System.out.println("Precio con descuento: $" + precioFinal);
+    }
+
+    // Verifica disponibilidad comparando con el stock actual [cite: 36]
+    public void verificarStock(int cantidad) {
+        if (cantidad <= stock) {
+            System.out.println("Disponible: " + stock);
+        } else {
+            System.out.println("Stock insuficiente.");
+            System.out.println("Disponible: " + stock);
+        }
+    }
 }
